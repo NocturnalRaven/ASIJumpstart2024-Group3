c341826cf6765be2c8d1f6e3db9c54303380d32a
Raven Mahinay <ravenmahinay0818@gmail.com>
2024-10-05 13:26:20 +0800

    updated MBooking.cs

--- ASI.Basecode.Data/Models/MBooking.cs
+++ ASI.Basecode.Data/Models/MBooking.cs
@@ -6,6 +6,14 @@ namespace ASI.Basecode.Data.Models
     public partial class MBooking
     {
         public int BookingId { get; set; }
+        public string UserId { get; set; }
+        public string RoomId { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime UpdatedTime { get; set; }
+        public string BookedBy { get; set; }
+        public DateTime BookedTime { get; set; }
         public string InsBy { get; set; }
         public DateTime InsDt { get; set; }
         public string UpdBy { get; set; }
