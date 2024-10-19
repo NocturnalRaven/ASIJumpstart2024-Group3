60f3497a3a30cb84074ec33952021bca76b27edf
CLAB3-18\db2 <alberinandus09@gmail.com>
2024-10-19 11:24:10 +0800

    Initial setup for room repository and service implementation

---
+++ ASI.Basecode.Data/Repositories/RoomRepository.cs
@@ -0,0 +1,16 @@
+﻿using ASI.Basecode.Data.Interfaces;
+using Basecode.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASI.Basecode.Data.Repositories
+{
+    public class RoomRepository : BaseRepository, IRoomRepository
+    {
+        public RoomRepository(IUnitOfWork unitOfWork) : base(unitOfWork) { }
+        
+    }
+}
