e30966b77e80433a072a8dd0114ca2caac2fd5c1
Alberin Andus <130210722+suprandus@users.noreply.github.com>
2024-10-21 00:18:23 +0800

    Update route naming convention to kebab case

--- ASI.Basecode.WebApp/Controllers/AdminDashboard.cs
+++ ASI.Basecode.WebApp/Controllers/AdminDashboard.cs
@@ -1,29 +1,77 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ASI.Basecode.Services.Interfaces;
+using ASI.Basecode.Services.ServiceModels;
+using ASI.Basecode.Services.Services;
+using ASI.Basecode.WebApp.Mvc;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
 
 namespace ASI.Basecode.WebApp.Controllers
 {
-    public class AdminDashboard : Controller
+    public class AdminDashboard : ControllerBase<AdminDashboard>
     {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="httpContextAccessor"></param>
+        /// <param name="loggerFactory"></param>
+        /// <param name="configuration"></param>
+        /// <param name="localizer"></param>
+        /// <param name="mapper"></param>
+
+        public AdminDashboard(
+           IHttpContextAccessor httpContextAccessor,
+                             ILoggerFactory loggerFactory,
+                             IConfiguration configuration,
+                             IMapper mapper = null) : base(httpContextAccessor, loggerFactory, configuration, mapper)
+        {
+
+        }
+
+
+        #region GET Methods
+
+        [HttpGet]
+        //[Authorize(Policy = "AdminOnly")]
+        [Route("/admin-dashboard/index")]
         public IActionResult Admindashboard()
         {
             return View();
         }
 
+        [HttpGet]
+        //[Authorize(Policy = "AdminOnly")]
+        [Route("/admin-dashboard/users")]
+        public IActionResult AdminUserDashboard()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        //[Authorize(Policy = "AdminOnly")]
+        [Route("/admin-dashboard/rooms")]
         public IActionResult Rooms()
         {
             return View();
         }
 
 
-        public IActionResult AdminUserDashboard()
-        {
-            return View();
-        }
-
+        [HttpGet]
+        //[Authorize(Policy = "AdminOnly")]
+        [Route("/admin-dashboard/analytics")]
         public IActionResult AdminAnalytics()
         {
 
             return View();
         }
+
+        #endregion
+
+
     }
 }
