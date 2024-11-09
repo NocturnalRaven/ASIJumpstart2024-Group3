5878039a94b8b3799f6741c8156cb868d141f01d
Raven Mahinay <ravenmahinay0818@gmail.com>
2024-11-09 15:32:37 +0800

    added validations for booking admin and SA

--- ASI.Basecode.Data/Interfaces/IBookingRepository.cs
+++ ASI.Basecode.Data/Interfaces/IBookingRepository.cs
@@ -14,5 +14,6 @@ namespace ASI.Basecode.Data.Interfaces
         void AddBooking(Booking booking);
         void UpdateBooking(Booking booking);
         void DeleteBooking(int bookingId);
+        int ArchiveExpiredBookings();
     }
 }
